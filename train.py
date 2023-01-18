87256882b46613a0d9cfa0cdde1304f5a5e0cf2e
Rohollah <rohollah903@gmail.com>
2023-01-18 17:49:12 +0330

    modifying saving

--- train.py
+++ train.py
@@ -41,8 +41,52 @@ from model import (
     encoder_scheduler,
     decoder_scheduler
 )
-from utils import SaveBestModel
 
+
+class SaveBestModel:
+    """
+    Class to save the best model while training. If the current iteration's
+    validation mAP is higher than the previous least less, then save the
+    model state.
+    """
+
+    def __init__(self, best_valid=1000):
+        self.best_valid = best_valid
+
+    def __call__(
+        self,
+        best_valid,
+        iteration,
+        encoder,
+        decoder,
+        encoder_optimizer,
+        decoder_optimizer,
+        voc,
+        embedding,
+        train_loss_per_iteration,
+        valid_loss_per_iteration,
+        encoder_scheduler,
+        decoder_scheduler,
+        directory
+    ):
+        if best_valid < self.best_valid:
+            self.best_valid = best_valid
+            torch.save(
+                {
+                    "iteration": iteration,
+                    "en": encoder.state_dict(),
+                    "de": decoder.state_dict(),
+                    "en_opt": encoder_optimizer.state_dict(),
+                    "de_opt": decoder_optimizer.state_dict(),
+                    "voc_dict": voc.__dict__,
+                    "embedding": embedding.state_dict(),
+                    "train_loss_per_iteration": train_loss_per_iteration,
+                    "valid_loss_per_iteration": valid_loss_per_iteration,
+                    "encoder_scheduler": encoder_scheduler.state_dict(),
+                    "decoder_scheduler": decoder_scheduler.state_dict()
+                },
+                os.path.join(directory, f"best_model_checkpoint.tar"),
+            )
 
 
 def maskNLLLoss(inp, target, mask):
@@ -196,7 +240,7 @@ def trainIters(
 
     directory = os.path.join(
         OUT_DIR,
-        model_name.replace(':', ''),
+        model_name.replace(', ', '-').replace(': ', '_').replace(',', '').strip(),
     )
     if not os.path.exists(directory):
         os.makedirs(directory)
--- utils.py
+++ utils.py
@@ -71,47 +71,4 @@ def display_valid_loss():
     plt.show()
 
 
-class SaveBestModel:
-    """
-    Class to save the best model while training. If the current iteration's
-    validation mAP is higher than the previous least less, then save the
-    model state.
-    """
 
-    def __init__(self, best_valid=1000):
-        self.best_valid = best_valid
-
-    def __call__(
-        self,
-        best_valid,
-        iteration,
-        encoder,
-        decoder,
-        encoder_optimizer,
-        decoder_optimizer,
-        voc,
-        embedding,
-        train_loss_per_iteration,
-        valid_loss_per_iteration,
-        encoder_scheduler,
-        decoder_scheduler,
-        directory
-    ):
-        if best_valid < self.best_valid:
-            self.best_valid = best_valid
-            torch.save(
-                {
-                    "iteration": iteration,
-                    "en": encoder.state_dict(),
-                    "de": decoder.state_dict(),
-                    "en_opt": encoder_optimizer.state_dict(),
-                    "de_opt": decoder_optimizer.state_dict(),
-                    "voc_dict": voc.__dict__,
-                    "embedding": embedding.state_dict(),
-                    "train_loss_per_iteration": train_loss_per_iteration,
-                    "valid_loss_per_iteration": valid_loss_per_iteration,
-                    "encoder_scheduler": encoder_scheduler.state_dict(),
-                    "decoder_scheduler": decoder_scheduler.state_dict()
-                },
-                os.path.join(directory, f"best_model_checkpoint.tar"),
-            )
